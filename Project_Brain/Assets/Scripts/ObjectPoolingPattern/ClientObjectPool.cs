fa2919fe788e8561566650d18ea9e97590d510c9
Ryutaeha <taeha61@gmail.com>
2024-06-17 07:08:07 +0900

    ObjectPoolingPattern

--- Project_Brain/Assets/Scripts/ObjectPoolingPattern/ClientObjectPool.cs
+++ Project_Brain/Assets/Scripts/ObjectPoolingPattern/ClientObjectPool.cs
@@ -5,18 +5,23 @@ using UnityEngine;
 public class ClientObjectPool : MonoBehaviour
 {
     #region Fields
-    private MonsterObjectPool _pool;
+    // MonsterObjectPool 객체를 참조하는 필드입니다.
+    private MonsterObjectPool _pool; 
     #endregion
 
     #region Unity Methods
+    // 게임 오브젝트에 MonsterObjectPool 컴포넌트를 추가합니다.
     private void Start()
     {
+        
         _pool = gameObject.AddComponent<MonsterObjectPool>();
     }
 
+    // "Spawn Monsters" 버튼을 생성하고, 클릭 시 몬스터를 스폰합니다.
     private void OnGUI()
     {
-        if(GUILayout.Button("Spawn Monsters")) _pool.Spawn();
+
+        if (GUILayout.Button("Spawn Monsters")) _pool.Spawn();
     }
     #endregion
 }
--- Project_Brain/Assets/Scripts/ObjectPoolingPattern/MonsterObjectPool.cs
+++ Project_Brain/Assets/Scripts/ObjectPoolingPattern/MonsterObjectPool.cs
@@ -1,14 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.Pool;
 
 public class MonsterObjectPool : MonoBehaviour
 {
     #region Fields
+    // 풀의 최대 크기입니다.
     public int maxPoolSize = 10;
+    // 풀의 기본 용량입니다.
     public int stackDefaultCapacity = 10;
 
+    // 객체 풀을 관리하는 인터페이스입니다.
     private IObjectPool<PoolingMonster> _pool;
     #endregion
 
@@ -17,13 +19,21 @@ public class MonsterObjectPool : MonoBehaviour
     {
         get
         {
-            if (_pool == null) _pool = new ObjectPool<PoolingMonster>(CreatedPooledItem, OnTakeFromPool, OnReturnedToPool, OnDestroyPoolObject, true, stackDefaultCapacity, maxPoolSize);
+            if (_pool == null) _pool = new ObjectPool<PoolingMonster>(
+                CreatedPooledItem,
+                OnTakeFromPool,
+                OnReturnedToPool,
+                OnDestroyPoolObject,
+                true,
+                stackDefaultCapacity,
+                maxPoolSize);
             return _pool;
         }
     }
     #endregion
 
     #region Methods
+    // 풀에 새로운 몬스터 객체를 생성하여 반환합니다.
     private PoolingMonster CreatedPooledItem()
     {
         var go = GameObject.CreatePrimitive(PrimitiveType.Cube);
@@ -36,26 +46,30 @@ public class MonsterObjectPool : MonoBehaviour
         return monster;
     }
 
+    // 풀로 반환된 몬스터 객체를 비활성화합니다.
     private void OnReturnedToPool(PoolingMonster monster)
     {
         monster.gameObject.SetActive(false);
     }
 
+    // 풀에서 가져온 몬스터 객체를 활성화합니다.
     private void OnTakeFromPool(PoolingMonster monster)
     {
         monster.gameObject.SetActive(true);
     }
 
+    // 풀에서 제거된 몬스터 객체를 파괴합니다.
     private void OnDestroyPoolObject(PoolingMonster monster)
     {
         Destroy(monster.gameObject);
     }
 
+    // 랜덤한 위치에 몬스터 객체를 생성하여 배치합니다.
     public void Spawn()
     {
         var amount = Random.Range(1, 10);
 
-        for(int i = 0; i < amount; i++)
+        for (int i = 0; i < amount; i++)
         {
             var monster = Pool.Get();
             monster.transform.position = Random.insideUnitSphere * 10;
