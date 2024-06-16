81496e38fd148c59ccb6cc147b1526d97b622d1f
Ryutaeha <taeha61@gmail.com>
2024-06-17 07:47:33 +0900

    Update Logger.cs

--- Project_Brain/Assets/Scripts/ServiceLocatorPattern/Services/Logger.cs
+++ Project_Brain/Assets/Scripts/ServiceLocatorPattern/Services/Logger.cs
@@ -1,14 +1,15 @@
 using ServiceInterface;
 using UnityEngine;
 
-
 public class Logger : ILoggerService
 {
     #region Methods
+    // ILoggerService 인터페이스의 Log 메서드를 구현한 것입니다.
+    // 주어진 메시지를 Unity의 디버그 로그에 출력합니다.
+    // 매개변수 message는 기록할 메시지입니다.
     public void Log(string message)
     {
         Debug.Log($"Logged: {message}");
     }
     #endregion
-
 }
